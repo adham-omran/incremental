5ffe0f8cb2a26d1930ca942b47b5501c4ca477b9
adham <adham@adhams-MacBook-Air.local>
2025-07-05 20:15:27 +0300

    Experimental PDF rendering

--- src/main/java/com/adham_omran/Incremental.java
+++ src/main/java/com/adham_omran/Incremental.java
@@ -19,11 +19,17 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import java.awt.MouseInfo;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 /**
  * JavaFX App
@@ -67,11 +73,17 @@ public class Incremental extends Application {
 
         PDDocument doc = Loader.loadPDF(pdfFile);
 
+        PDFRenderer rndr = new PDFRenderer(doc);
+        System.out.println(rndr.renderImage(0).getClass());
+        System.out.println(bufferedImageToFXImage(rndr.renderImage(0)).getClass());
+
         // Image Viewing
 
         Image img = new Image(getClass().getResourceAsStream("/image.jpg"));
+        // img = rndr.renderImage(0);
 
         ImageView iv1 = new ImageView(img);
+        ImageView iv2 = new ImageView(bufferedImageToFXImage(rndr.renderImageWithDPI(0, 300)));
 
         btn.setOnAction(event -> {
                 System.out.println(MouseInfo.getPointerInfo().getLocation());
@@ -85,11 +97,27 @@ public class Incremental extends Application {
         gp.add(xLabel, 0, 1);
         gp.add(xField, 1, 1);
         gp.add(btn, 0, 3);
-        gp.add(iv1, 0, 4);
+        gp.add(iv2, 0, 4);
 
         // var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(new Scene(gp, 640, 480));
         stage.show();
+    }
+
+    public static Image bufferedImageToFXImage(BufferedImage bufferedImage) {
+        try {
+            // Convert BufferedImage to byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", baos);
+            byte[] imageBytes = baos.toByteArray();
+
+            // Create JavaFX Image from byte array
+            ByteArrayInputStream bais = new ByteArrayInputStream(imageBytes);
+            return new Image(bais);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to convert BufferedImage to JavaFX Image", e);
+        }
     }
 
     public static void main(String[] args) {
