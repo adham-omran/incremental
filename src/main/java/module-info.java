0ea7479d83f1b872835c49fdd15a571b53b534da
Adham Omran <git@adham-omran.com>
2025-07-13 21:55:36 +0300

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.adham_omran {
     requires javafx.swing;
     requires transitive javafx.graphics;
     requires transitive java.desktop;
-    requires java.sql;
+    requires transitive java.sql;
     requires org.apache.pdfbox;
     exports com.adham_omran;
 }
