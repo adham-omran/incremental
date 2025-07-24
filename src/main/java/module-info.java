d9e3f43176a7b6e735dd13679abcb7e16ffa7f9e
Adham Omran <git@adham-omran.com>
2025-07-24 20:26:42 +0300

    Add requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,6 @@ module com.adham_omran {
     requires transitive java.sql;
     requires org.apache.pdfbox;
     requires transitive jfx.incubator.richtext;
+    requires com.dlsc.pdfviewfx;
     exports com.adham_omran;
 }
